84d10dc698843ed24de6314fb3cbf5b9234f9967
hqvinh040589@gmail.com <hqvinh040589@gmail.com@0dd4d5a2-1872-0262-5223-05bcef6ff0c2>
2012-08-23 19:28:30 +0000

    show img success

--- trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/ImageCategoryFacade.java
+++ trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/ImageCategoryFacade.java
@@ -5,9 +5,12 @@
 package com.mysweethome.session;
 
 import com.mysweethome.entity.ImageCategory;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -41,4 +44,32 @@ public class ImageCategoryFacade extends AbstractFacade<ImageCategory> {
     public void remove(ImageCategory imagecategory) {
         em.remove(em.merge(imagecategory));
     }  
+    
+    public List<String> getImagesOfEstate(String estateID){
+        List<String> images=new ArrayList<String>();
+        Query q=em.createNamedQuery("ImageCategory.findByImageCategoryID");
+        q.setParameter("imageCategoryID", estateID);
+        ImageCategory ct=(ImageCategory) q.getSingleResult();
+        if (ct != null) {
+                if(ct.getImage1()!=null && ct.getImage1().length()>0){
+                    images.add(ct.getImage1());
+                }
+                if(ct.getImage2()!=null && ct.getImage2().length()>0){
+                    images.add(ct.getImage2());
+                }
+                if(ct.getImage3()!=null && ct.getImage3().length()>0){
+                    images.add(ct.getImage3());
+                }
+                if(ct.getImage4()!=null && ct.getImage4().length()>0){
+                    images.add(ct.getImage4());
+                }
+                if(ct.getImage5()!=null && ct.getImage5().length()>0){
+                    images.add(ct.getImage5());
+                }
+                if(ct.getImage6()!=null && ct.getImage6().length()>0){
+                    images.add(ct.getImage6());
+                }
+        }
+        return images;
+    }
 }
