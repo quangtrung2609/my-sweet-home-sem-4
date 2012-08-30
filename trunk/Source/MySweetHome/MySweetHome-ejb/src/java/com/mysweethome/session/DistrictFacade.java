2b2faa9343fbd1ad98e88c10b314cb18ef346303
vulongkd10@gmail.com <vulongkd10@gmail.com@0dd4d5a2-1872-0262-5223-05bcef6ff0c2>
2012-08-30 11:42:35 +0000

    sua sessionBean CityFacade, FAQsFacade va DistrictFacade

--- trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/DistrictFacade.java
+++ trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/DistrictFacade.java
@@ -4,7 +4,9 @@
  */
 package com.mysweethome.session;
 
+import com.mysweethome.entity.City;
 import com.mysweethome.entity.District;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +18,7 @@ import javax.persistence.Query;
  */
 @Stateless
 public class DistrictFacade extends AbstractFacade<District> {
+
     @PersistenceContext(unitName = "MySweetHome-ejbPU")
     private EntityManager em;
 
@@ -27,7 +30,7 @@ public class DistrictFacade extends AbstractFacade<District> {
     public DistrictFacade() {
         super(District.class);
     }
-    
+
     @Override
     public void create(District district) {
         em.persist(district);
@@ -44,27 +47,55 @@ public class DistrictFacade extends AbstractFacade<District> {
     }
 
     public District getDistrictID(String districtID) {
-        District ds=null;
-        try{
-            Query query=em.createNamedQuery("District.findByDistrictID");
+        District ds = null;
+        try {
+            Query query = em.createNamedQuery("District.findByDistrictID");
             query.setParameter("districtID", districtID);
-            ds=(District) query.getSingleResult();
-            
-        }catch(Exception ex){
+            ds = (District) query.getSingleResult();
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return ds;
     }
+
     public District getDistrictName(String districtName) {
-        District ds=null;
-        try{
-            Query query=em.createNamedQuery("District.findByDistrictName");
+        District ds = null;
+        try {
+            Query query = em.createNamedQuery("District.findByDistrictName");
             query.setParameter("districtName", districtName);
-            ds=(District) query.getSingleResult();
-            
-        }catch(Exception ex){
+            ds = (District) query.getSingleResult();
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return ds;
+    }
+
+    public City getCityFromID(String id) {
+        Query query = em.createNamedQuery("City.findByCityID");
+        query.setParameter("cityID", id);
+        try{
+            City city=new City();
+            city=(City) query.getResultList().get(0);
+            return city;
+        }catch(Exception e){return null;}
+    }
+    public int getLastRecordID(){
+        Query q= em.createNamedQuery("District.findAll");
+        List<District> distlist= q.getResultList();
+                
+        int i=distlist.size();
+        i=i-1;
+        
+        int j=Integer.parseInt(distlist.get(i).getDistrictID());
+        return j;
+    }
+    public List<City> getListCity(){
+        
+        Query query = em.createNamedQuery("City.findAll");
+            
+            return query.getResultList();
+        
     }
 }
