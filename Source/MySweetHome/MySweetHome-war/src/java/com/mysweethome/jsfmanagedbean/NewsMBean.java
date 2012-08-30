e2afc3819f5313ad2de50e76adf0e254e47dfe06
hqvinh040589@gmail.com <hqvinh040589@gmail.com@0dd4d5a2-1872-0262-5223-05bcef6ff0c2>
2012-08-30 09:57:57 +0000

    detail table hoàn thành

---
+++ Source/MySweetHome/MySweetHome-war/src/java/com/mysweethome/jsfmanagedbean/NewsMBean.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mysweethome.jsfmanagedbean;
+
+import com.mysweethome.entity.News;
+import com.mysweethome.session.NewsFacade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Welcome
+ */
+
+@ManagedBean(name="newsMBean")
+@SessionScoped
+public class NewsMBean {
+    @EJB
+    public NewsFacade newsFacade;
+    public News news, other;
+    public String newsID;
+    public String newsTitle;
+    public String newsContent;
+    public List<News> getNewsList;
+
+    public List<News> getGetNewsList() {
+        return newsFacade.findAll();
+    }
+
+    public void setGetNewsList(List<News> getNewsList) {
+        this.getNewsList = getNewsList;
+    }
+    
+
+    public News getNews() {
+        return news;
+    }
+
+    public void setNews(News news) {
+        this.news = news;
+    }
+
+    public String getNewsContent() {
+        return newsContent;
+    }
+
+    public void setNewsContent(String newsContent) {
+        this.newsContent = newsContent;
+    }
+
+    public NewsFacade getNewsFacade() {
+        return newsFacade;
+    }
+
+    public void setNewsFacade(NewsFacade newsFacade) {
+        this.newsFacade = newsFacade;
+    }
+
+    public String getNewsID() {
+        return newsID;
+    }
+
+    public void setNewsID(String newsID) {
+        this.newsID = newsID;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+    
+    
+    /**
+     * Creates a new instance of NewsMBean
+     */
+    public NewsMBean() {
+        newsFacade = new NewsFacade();
+        news = new News();
+        other = new News();
+        newsID = news.getNewsID();
+        newsTitle = news.getNewsTitle();
+        newsContent = news.getNewsContents();
+        getNewsList = new ArrayList<News>();
+    }
+    
+    
+    
+    public void createNews() {
+        
+        
+        try{
+            News otherNews = new News();
+            //newSub = subfacade.find(subscribeID);
+        
+            int id= getNewsFacade().getLastRecordID();
+            otherNews.setNewsID(String.valueOf(id+1));
+            otherNews.setNewsTitle(other.getNewsTitle());
+            otherNews.setNewsContents(other.getNewsContents());
+            
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+              
+    }
+    
+    
+    public void editNews(){
+        
+        String str = getNews().getNewsID();
+        News editNews = getNewsFacade().getNewsID(str);
+        editNews.setNewsID(newsID);
+        editNews.setNewsTitle(newsTitle);
+        editNews.setNewsContents(newsContent);
+        
+        getNewsFacade().edit(editNews);
+    }
+    
+    
+    public void removeNews() {
+        String str = getNews().getNewsID();
+        getNews().setNewsID(str);
+        getNewsFacade().remove(getNews());
+    }
+    
+    
+    
+    
+    
+}
