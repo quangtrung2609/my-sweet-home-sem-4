bedbd1caaaa1f3311c865d007f9e1aaf4d09ee0c
hqvinh040589@gmail.com <hqvinh040589@gmail.com@0dd4d5a2-1872-0262-5223-05bcef6ff0c2>
2012-08-30 09:59:55 +0000

    xu ly detail table cho ejb (chua hoan tat phan dialog)

--- trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/NewsFacade.java
+++ trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/NewsFacade.java
@@ -5,9 +5,11 @@
 package com.mysweethome.session;
 
 import com.mysweethome.entity.News;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -41,4 +43,54 @@ public class NewsFacade extends AbstractFacade<News> {
     public void remove(News news) {
         em.remove(em.merge(news));
     }  
+    
+    
+    public News getNewsID(String newsID){
+        News news1=null;
+        try{
+            Query query=em.createNamedQuery("News.findByNewsID");
+            query.setParameter("newsID", newsID);
+            news1=(News) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return news1;
+    }
+    
+    public News getNewsTitle(String newsTitle){
+        News news1=null;
+        try{
+            Query query=em.createNamedQuery("News.findByNewsTitle");
+            query.setParameter("newsTitle", newsTitle);
+            news1=(News) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return news1;
+    }
+    
+    public News getNewsContent(String newsContents){
+        News news1=null;
+        try{
+            Query query=em.createNamedQuery("News.findByNewsContents");
+            query.setParameter("newsContents", newsContents);
+            news1=(News) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return news1;
+    }
+    
+    
+    public int getLastRecordID(){
+        Query q= em.createNamedQuery("News.findAll");
+        List<News> newsList= q.getResultList();
+                
+        int i=newsList.size();
+        i=i-1;
+        
+        int j=Integer.parseInt(newsList.get(i).getNewsID());
+        return j;
+    }
+    
 }
--- trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/SubscribeFacade.java
+++ trunk/Source/MySweetHome/MySweetHome-ejb/src/java/com/mysweethome/session/SubscribeFacade.java
@@ -5,9 +5,11 @@
 package com.mysweethome.session;
 
 import com.mysweethome.entity.Subscribe;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -17,6 +19,7 @@ import javax.persistence.PersistenceContext;
 public class SubscribeFacade extends AbstractFacade<Subscribe> {
     @PersistenceContext(unitName = "MySweetHome-ejbPU")
     private EntityManager em;
+   
 
     @Override
     protected EntityManager getEntityManager() {
@@ -41,4 +44,52 @@ public class SubscribeFacade extends AbstractFacade<Subscribe> {
     public void remove(Subscribe subscribe) {
         em.remove(em.merge(subscribe));
     }  
+    
+    public Subscribe getSubscribeID(String subscribeID){
+        Subscribe sub=null;
+        try{
+            Query query=em.createNamedQuery("Subscribe.findBySubscribeID");
+            query.setParameter("subscribeID", subscribeID);
+            sub=(Subscribe) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return sub;
+    }
+    
+    public Subscribe getSubscribeName(String subscribeName){
+        Subscribe sub=null;
+        try{
+            Query query=em.createNamedQuery("Subscribe.findBySubscribeName");
+            query.setParameter("subscribeName", subscribeName);
+            sub=(Subscribe) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return sub;
+    }
+    
+    public Subscribe getFeePerDay(String feePerDay){
+        Subscribe sub=null;
+        try{
+            Query query=em.createNamedQuery("Subscribe.findByFeePerDay");
+            query.setParameter("feePerDay", feePerDay);
+            sub=(Subscribe) query.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return sub;
+    }
+    
+    public int getLastRecordID(){
+        Query q= em.createNamedQuery("Subscribe.findAll");
+        List<Subscribe> sublist= q.getResultList();
+                
+        int i=sublist.size();
+        i=i-1;
+        
+        int j=Integer.parseInt(sublist.get(i).getSubscribeID());
+        return j;
+    }
+    
 }
