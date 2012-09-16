49a6a574e92bb2aa12c0dd2a77160bf14ac9c70f
quanghuy9289 <quanghuy9289@0dd4d5a2-1872-0262-5223-05bcef6ff0c2>
2012-09-16 14:54:25 +0000

    city

--- Source/MySweetHome/MySweetHome-war/src/java/com/mysweethome/jsfmanagedbean/CityMBean.java
+++ Source/MySweetHome/MySweetHome-war/src/java/com/mysweethome/jsfmanagedbean/CityMBean.java
@@ -2,14 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mysweethome.jsfmanagedbean;
 
 import com.mysweethome.entity.City;
+import com.mysweethome.helper.messages;
 import com.mysweethome.session.CityFacade;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,17 +18,48 @@ import javax.faces.bean.SessionScoped;
  *
  * @author cp201008e8508
  */
-@ManagedBean (name = "CityMBean")
+@ManagedBean(name = "CityMBean")
 @SessionScoped
 public class CityMBean {
+
     @EJB
     private CityFacade cityFacade;
     private City city;
     private City city2;
+    private City city3;
+
+    public City getCity3() {
+        return city3;
+    }
+
+    public void setCity3(City city3) {
+        this.city3 = city3;
+    }
     String name;
     String cityID;
     List<City> citylist;
     List<City> filteredCity;
+    String newID;
+    String newCityName;
+
+    public String getNewCityName() {
+        return newCityName;
+    }
+
+    public void setNewCityName(String newCityName) {
+        this.newCityName = newCityName;
+    }
+
+    public String getNewID() {
+        int id = getCityFacade().getLastRecordID();
+        id += 1;
+        newID = String.valueOf(id);
+        return newID;
+    }
+
+    public void setNewID(String newID) {
+        this.newID = newID;
+    }
 
     public String getCityID() {
         return city.getCityID();
@@ -44,6 +76,7 @@ public class CityMBean {
     public void setName(String name) {
         this.name = name;
     }
+
     public City getCity2() {
         return city2;
     }
@@ -51,8 +84,6 @@ public class CityMBean {
     public void setCity2(City city2) {
         this.city2 = city2;
     }
-
-    
 
     public List<City> getCitylist() {
         return citylist = cityFacade.findAll();
@@ -69,7 +100,7 @@ public class CityMBean {
     public void setCitylist(List<City> citylist) {
         this.setCitylist(citylist);
     }
-    
+
     public CityFacade getCityFacade() {
         return cityFacade;
     }
@@ -77,6 +108,7 @@ public class CityMBean {
     public void setCityFacade(CityFacade cityFacade) {
         this.cityFacade = cityFacade;
     }
+
     public City getCity() {
         return city;
     }
@@ -84,14 +116,15 @@ public class CityMBean {
     public void setCity(City city) {
         this.city = city;
     }
-    
 
-    /** Creates a new instance of CityMBean */
+    /**
+     * Creates a new instance of CityMBean
+     */
     public CityMBean() {
-        city=new City();
+        city = new City();
         city2 = new City();
-        cityFacade=new CityFacade();
-        name=city.getCityName();
+        cityFacade = new CityFacade();
+        name = city.getCityName();
         citylist = new ArrayList<City>();
         filteredCity = new ArrayList<City>();
     }
@@ -99,55 +132,47 @@ public class CityMBean {
 //        city.getCityID();
 //        city.getCityName();
 //    }
-    public String createCity(){
-        City citytemp= new City();
-        
-        String result="False";
-        try{
-        int id= getCityFacade().getLastRecordID();
-        citytemp.setCityID(String.valueOf(id+1));
-        citytemp.setCityName(city2.getCityName());        
-        
-        getCityFacade().create(citytemp);
-        result="True";
-        }catch(Exception ex){
-            ex.printStackTrace();
+
+    public void createCity() {
+        if (city2.getCityID() != null) {
+            City temp = new City();
+            temp = cityFacade.getCityByID(city2.getCityID());
+            if (temp != null) {
+                messages.taoTB(FacesMessage.SEVERITY_WARN, "Duplicate cityID!", "This cityID had already");
+            } else {
+                City citytemp = new City();
+                citytemp.setCityID(city2.getCityID());
+                citytemp.setCityName(city2.getCityName());
+                getCityFacade().create(citytemp);
+                messages.taoTB(FacesMessage.SEVERITY_INFO, "Create city success!", "Create city success!");
+            }
         }
-        return result;       
+        this.city2 = new City();
     }
-    public void editCity(){
-        
-        String str=getCity().getCityID();
-        City ct=getCityFacade().getCityID(str);
-        ct.setCityName(name);
-        
-        
-        getCityFacade().edit(ct);
+
+    public void editCity(City city) {
+        getCityFacade().edit(city);
+        messages.taoTB(FacesMessage.SEVERITY_INFO, "Edit success", "Edit success");
+        this.city = new City();
     }
-      public void editCity(City city1){                  
-        
-        getCityFacade().edit(city1);
+
+    public void removeCity(String cityID) {
+        if (cityID != null) {
+            City temp = new City();
+            temp = cityFacade.getCityByID(city.getCityID());
+            getCityFacade().remove(temp);
+            messages.taoTB(FacesMessage.SEVERITY_INFO, "Delete success", "Delete success");
+        }
+        this.city = new City();
     }
-    public void removeCity(){
-        String str=getCity().getCityID();
-        getCity().setCityID(str);
-        getCityFacade().remove(getCity());
-    }
-     public void removeCity(String cityID){
-        City citytemp=this.getCityFacade().find(cityID);
-        getCityFacade().remove(citytemp);
-    }
-    public List<City> getAllCity(){
+
+    public List<City> getAllCity() {
         return getCityFacade().findAll();
     }
 
-    public String getCityNameFromID(){
-        String str=city.getCityID();
-        city2= cityFacade.find(str); 
+    public String getCityNameFromID() {
+        String str = city.getCityID();
+        city2 = cityFacade.find(str);
         return city2.getCityName();
     }
-
-    
-    
-
 }
